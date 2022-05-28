c80d10580cf320fdc7426b54ccf611194baa9b86
Taras Prystuplyk <1988taras@ukr.net>
2022-05-29 00:09:23 +0300

    ffix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from '.src/fetchCountries'; 
+import { fetchCountries } from './fetchCountries'; 
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
